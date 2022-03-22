20e97b07abb6e819b67352ee0b9cd302e1c44ad3
Dilyara44 <100514175+Dilyara44@users.noreply.github.com>
2022-03-22 15:44:59 +0300

    Easy code added

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,69 +1,30 @@
 package ru.netology
 
 fun main() {
-    val typeOfCard = listOf("MasterCard", "VISA", "Maestro", "Мир", "VK Pay").random()
-    val sumOfPreviousTransfers = (0..700000).random()
-    val transactionSum = (0..200000).random()
-    val sumOfCommissionForVISA = 0.75
-
-    println(typeOfCard)
-    println(sumOfPreviousTransfers)
-    println(transactionSum)
-    println(" ")
-    countCommission(typeOfCard, sumOfPreviousTransfers, transactionSum, sumOfCommissionForVISA)
-
+    println(countCommission("MasterCard", 23000.00, 1233.00))
 }
 
 
 fun countCommission(
     typeOfCard: String = "VK Pay",
-    sumOfPreviousTransfers: Int = 0,
-    transactionSum: Int,
-    sumOfCommissionForVISA: Double
-) {
-    when (limits(sumOfPreviousTransfers, transactionSum, typeOfCard)) {
-        true ->
-            when (typeOfCard) {
-
-                "MasterCard", "Maestro" -> if (sumOfPreviousTransfers + transactionSum < 75_000) {
-                    println("В рамках акции комиссия не взимается. Сумма перевода: $transactionSum.")
-                } else {
-                    val commission = transactionSum * 0.6 / 100 + 20
-                    val amount = transactionSum - commission
-                    println("Сумма перевода: $amount. Сумма комиссии: $commission.")
-                }
-
-                "VISA", "Мир" ->
-                    if ((countCommissionForVISA(sumOfCommissionForVISA, transactionSum)) < 35) {
-                        val commission = 35
-                        val amount = transactionSum - commission
-                        println("Сумма перевода: $amount. Сумма комиссии: $commission. ")
-                    } else {
-                        val commission = countCommissionForVISA(sumOfCommissionForVISA, transactionSum)
-                        val amount = transactionSum - commission
-                        println("Сумма перевода: $amount. Сумма комиссии: $commission.")
-                    }
-
-                "VK Pay" ->
-                    println("Комиссия не взимается. Сумма перевода: $transactionSum")
+    sumOfPreviousTransfers: Double = 0.00,
+    transactionSum: Double,
+): Double {
+    return when (typeOfCard) {
+        "MasterCard", "Maestro" -> if (sumOfPreviousTransfers + transactionSum < 75_000) {
+            0.00
+        } else {
+            val commission = transactionSum * 0.6 / 100 + 20
+            commission
+        }
+        "VISA", "Мир" ->
+            if ((transactionSum * 0.75 / 100) < 35) {
+                val commission = 35.00
+                commission
+            } else {
+                return transactionSum * 0.75 / 100
             }
 
-        false ->
-            println("Лимиты превышены. Попробуйте другой способ перевода.")
-
-
+        else -> 0.00
     }
-}
-
-fun limits(sumOfPreviousTransfers: Int, transactionSum: Int, typeOfCard: String): Boolean {
-    when (typeOfCard) {
-        "MasterCard", "Maestro", "VISA", "Мир" ->
-            return transactionSum < 150_000 && transactionSum + sumOfPreviousTransfers < 600_000
-        "VK Pay" -> return transactionSum < 15_000 && transactionSum + sumOfPreviousTransfers < 40_000
-    }
-    return false
-}
-
-fun countCommissionForVISA(sumOfCommissionForVISA: Double, transactionSum: Int): Double {
-    return transactionSum * sumOfCommissionForVISA / 100
 }
